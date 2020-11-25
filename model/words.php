7348bde7c6bc73ac31e5458c0804a7a40fe6d24a
caoting <307298900@qq.com>
2020-11-25 18:23:37 +0800

    save

--- model/words.php
+++ model/words.php
@@ -7,4 +7,26 @@ use tools\model;
 class Words extends model
 {
     protected static $table = 'words';
+
+    public static function getWords($words)
+    {
+        array_walk($words, function (&$e) {
+            $e = "'$e'";
+        });
+
+        $words = implode(", ", $words);
+        $sql = "SELECT * FROM words where word in ({$words})";
+        $query = self::db()->prepare($sql);
+
+        $query->execute();
+        return $query->fetchAll(self::db()::FETCH_ASSOC);
+    }
+
+    public static function incr($id)
+    {
+        $sql = "UPDATE words SET `frequency`=`frequency`+1 where id = '{$id}'";
+
+        $query = self::db()->prepare($sql);
+        $query->execute();
+    }
 }
